90e1bc844a30949936cdee1cd8256d25a3e91156
Sirshkoirala <seris.koerala@gmail.com>
2024-03-30 16:12:44 +0545

    optimization done

--- index.js
+++ index.js
@@ -11,52 +11,41 @@ let c = 8;
 let cpu = Math.floor(Math.random() * 3);
 let gameSet = ['r', 'p', 's'];
 cpu = gameSet[cpu];
+let user;
 
 while (c > 0) {
     console.log("                     ");
     console.log("You have " + c + " chances");
     c--;
-    let user = prompt("Enter your choice : >");
+    user = prompt("Enter your choice : >");
     if ((user === "r" && cpu === "r") || (user === "p" && cpu === "p") || (user === "s" && cpu === "s")) {
         console.log("Game tied");
-        console.log("CPU: " + cpu);
-        console.log("User: " + user);
     }
     if (user === "r" && cpu === "s") {
         console.log("User won!!");
-        console.log("CPU: " + cpu);
-        console.log("User: " + user);
         break;
     }
     if (user === "r" && cpu === "p") {
         console.log("CPU won!!");
-        console.log("CPU: " + cpu);
-        console.log("User: " + user);
         break;
     }
     if (user === "p" && cpu === "s") {
         console.log("Cpu won!!");
-        console.log("CPU: " + cpu);
-        console.log("User: " + user);
         break;
     }
     if (user === "p" && cpu === "r") {
         console.log("User won!!");
-        console.log("CPU: " + cpu);
-        console.log("User: " + user);
         break;
     }
     if (user === "s" && cpu === "r") {
         console.log("CPU won!!");
-        console.log("CPU: " + cpu);
-        console.log("User: " + user);
         break;
     }
     if (user === "s" && cpu === "p") {
         console.log("User won!!");
-        console.log("CPU: " + cpu);
-        console.log("User: " + user);
         break;
     }
 }
+console.log("CPU: " + cpu);
+console.log("User: " + user);
 
